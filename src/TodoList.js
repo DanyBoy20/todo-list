b068d72392d0abf93444d272d4d2a98e095dac11
Dany Hernandez <foxdan20@gmail.com>
2021-12-07 17:51:37 -0600

    INITIAL FILES

---
+++ src/TodoList.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import NewTodoForm from './NewTodoForm';
+import Todo from './Todo';
+import './TodoList.css';
+
+class TodoList extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            /* todos: [ {task: "Pasear perro"}, {task: "alimentar gato"} ] */ // tareas de ejemplo al iniciar
+            todos: []
+        }; 
+        this.create = this.create.bind(this);
+        this.remove = this.remove.bind(this);
+        this.update = this.update.bind(this);
+        this.toggleCompletion = this.toggleCompletion.bind(this);
+    }
+    create(newTodo){
+        this.setState({
+            todos : [...this.state.todos, newTodo]
+        })
+    }
+    remove(id){
+        this.setState({
+            todos: this.state.todos.filter(t => t.id !== id)
+        });
+    }
+    update(id, updatedTask){
+        const updatedTodos = this.state.todos.map(todo => {
+            // si el id del todo es igual al id que se recibe, se actualiza la tarea
+            if(todo.id === id){
+                return {...todo, task: updatedTask}
+            }
+            return todo;
+        });
+        this.setState({  todos: updatedTodos});
+    }
+    toggleCompletion(id){
+        const updatedTodos = this.state.todos.map(todo => {
+            if(todo.id === id){
+                return {...todo, completed: !todo.completed}
+            }
+            return todo;
+        });
+        this.setState({  todos: updatedTodos});        
+    }
+    render(){
+        const todos = this.state.todos.map(todo => {
+            return (
+                <Todo 
+                    key={todo.id} 
+                    id={todo.id} 
+                    task={todo.task} 
+                    completed={todo.completed}
+                    removeTodo={this.remove} 
+                    updateTodo={this.update}
+                    toggleTodo={this.toggleCompletion}
+                />
+            );
+        });  ;
+        return(
+            <div className="TodoList">
+                <h1>React Todo List <span>Simple Todo List App Build with React | DHW</span></h1>                
+                {/* LISTA DE TAREAS (TODOS) */}
+                <ul>
+                    {todos}
+                </ul>
+                <NewTodoForm createTodo={this.create} />
+            </div>
+        );
+    }
+}
+
+export default TodoList;
